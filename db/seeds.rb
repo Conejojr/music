b4002947b6c72bd4db4036a8c4364d2ebfddb073
kiyoshi <kiyo7s@icloud.com>
2019-10-11 21:09:57 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -112,217 +112,217 @@ Tune.create!(
 )
 
 
-# Blog.create!(
-#   [
-#     {
-#       video_url: 'https://www.youtube.com/embed/yzTuBuRdAyA',
-#       title: 'the Weeknd the hills',
-#       text: "Your man on the road, he doin' promo
-#       You said, 'Keep our business on the low-low'
-#       I'm just tryna get you out the friend zone
-#       ‘Cause you look even better than the photos
-#       I can't find your house, send me the info
+Blog.create!(
+  [
+    {
+      video_url: 'https://www.youtube.com/embed/yzTuBuRdAyA',
+      title: 'the Weeknd the hills',
+      text: "Your man on the road, he doin' promo
+      You said, 'Keep our business on the low-low'
+      I'm just tryna get you out the friend zone
+      ‘Cause you look even better than the photos
+      I can't find your house, send me the info
       
-#       Drivin' through the gated residential
-#       Found out I was comin', sent your friends home
-#       Keep on tryna hide it but your friends know
+      Drivin' through the gated residential
+      Found out I was comin', sent your friends home
+      Keep on tryna hide it but your friends know
       
-#       I only call you when it's half past five
-#       The only time that I'll be by your side
-#       I only love it when you touch me, not feel me
-#       When I'm fucked up, that's the real me
-#       When I'm fucked up, that's the real me, yeah
-#       I only call you when it's half past five
-#       The only time I'd ever call you mine
+      I only call you when it's half past five
+      The only time that I'll be by your side
+      I only love it when you touch me, not feel me
+      When I'm fucked up, that's the real me
+      When I'm fucked up, that's the real me, yeah
+      I only call you when it's half past five
+      The only time I'd ever call you mine
       
-#       I only love it when you touch me, not feel me
-#       When I'm fucked up, that's the real me
-#       When I'm fucked up, that's the real me, babe",
-#       image: "https://images-na.ssl-images-amazon.com/images/I/91lktlYc0HL._SX466_.jpg",
-#       created_at: "2019-10-02 02:47:13",
-#     },
-#     {
-#       video_url: 'https://www.youtube.com/embed/lzQpS1rH3zI',
-#       title: 'Juice WRLD',
-#       text: "I been living fast, fast, fast, fast
-#       Feeling really bad, bad, bad, bad
-#       Time really moves fast, fast, fast, fast
-#       Better hurry up and get in your bag, bag, bag, bag
-#       I wear Dior, not a fad, 'ad, ’ad, 'ad
+      I only love it when you touch me, not feel me
+      When I'm fucked up, that's the real me
+      When I'm fucked up, that's the real me, babe",
+      image: "https://images-na.ssl-images-amazon.com/images/I/91lktlYc0HL._SX466_.jpg",
+      created_at: "2019-10-02 02:47:13",
+    },
+    {
+      video_url: 'https://www.youtube.com/embed/lzQpS1rH3zI',
+      title: 'Juice WRLD',
+      text: "I been living fast, fast, fast, fast
+      Feeling really bad, bad, bad, bad
+      Time really moves fast, fast, fast, fast
+      Better hurry up and get in your bag, bag, bag, bag
+      I wear Dior, not a fad, 'ad, ’ad, 'ad
       
-#       I know all these niggas gettin' mad, mad, mad, mad
-#       My hand on my trigger, I’ma die with respect, yeah
-#       Fucking with my money, you'll get dealt like that, yeah
+      I know all these niggas gettin' mad, mad, mad, mad
+      My hand on my trigger, I’ma die with respect, yeah
+      Fucking with my money, you'll get dealt like that, yeah
       
       
-#       I took too many pills, count up the bills, uh
-#       Molly in my cup, I can't tell you how I feel, uh
-#       Oh, last call
-#       Oh-oh, it's gnarly
-#       Every day I be counting up, counting up the blues
-#       Gotta win sometimes when you always lose
-#       I get high as a bitch, still the same dude
-#       I was back then, and now I'm lost and confused",
-#       image: "https://tower.jp/article/feature_item/2019/03/28/~/media/3332612C874648E4B7B6A2180906B47C.ashx",
-#       created_at: "2019-10-03 05:52:53",
-#     },
-#     {
-#       video_url: 'https://www.youtube.com/embed/KnZ8h3MRuYg',
-#       title: 'travis scott Antidote',
-#       text: "Don't you open up that window
-#       Don't you let out that antidote
-#       Poppin' pills is all we know
-#       In the hills is all we know （Hollywood!）
-#       Don't go through the front door （through the back!）
-#       It's lowkey at the night show
-#       So don't you open up that window
-#       Don't you let out that antidote
+      I took too many pills, count up the bills, uh
+      Molly in my cup, I can't tell you how I feel, uh
+      Oh, last call
+      Oh-oh, it's gnarly
+      Every day I be counting up, counting up the blues
+      Gotta win sometimes when you always lose
+      I get high as a bitch, still the same dude
+      I was back then, and now I'm lost and confused",
+      image: "https://tower.jp/article/feature_item/2019/03/28/~/media/3332612C874648E4B7B6A2180906B47C.ashx",
+      created_at: "2019-10-03 05:52:53",
+    },
+    {
+      video_url: 'https://www.youtube.com/embed/KnZ8h3MRuYg',
+      title: 'travis scott Antidote',
+      text: "Don't you open up that window
+      Don't you let out that antidote
+      Poppin' pills is all we know
+      In the hills is all we know （Hollywood!）
+      Don't go through the front door （through the back!）
+      It's lowkey at the night show
+      So don't you open up that window
+      Don't you let out that antidote
       
-#       Party on a Sunday （that was fun!）
-#       Do it all again on Monday （one more time!）
-#       Spent a check on a weekend （oh my God!）
-#       I might do it all again （that's boss shit）
+      Party on a Sunday （that was fun!）
+      Do it all again on Monday （one more time!）
+      Spent a check on a weekend （oh my God!）
+      I might do it all again （that's boss shit）
       
-#       I just hit a three peat
-#       F*cked three hoes I met this week （Robert Horry!）
-#       I don't do no old hoes （oh, no, no!）
-#       My nigga, that's a no-no （straight up!）
-#       She just want the coco （Cocaina!）
-#       I just want dinero （dinero）
+      I just hit a three peat
+      F*cked three hoes I met this week （Robert Horry!）
+      I don't do no old hoes （oh, no, no!）
+      My nigga, that's a no-no （straight up!）
+      She just want the coco （Cocaina!）
+      I just want dinero （dinero）
       
-#       Who that at the front door?
-#       If it's the feds, oh-no-no-no （don't let 'em in, shhh）",
-#       image: "https://images-na.ssl-images-amazon.com/images/I/71RDpVmr2hL._SY355_.jpg",
-#       created_at: "2019-10-04 05:52:53",
-#     },
-#     {
-#       video_url: 'https://www.youtube.com/embed/K_9tX4eHztY',
-#       title: 'future ELVO',
-#       text: "A question that might come to mind when 
-#       analyzing trap music is whether the lyrical content is in fact an
-#       accurate representation of the rapper's selves and their views. 
-#       With this question in mind, what can one make of Future?
+      Who that at the front door?
+      If it's the feds, oh-no-no-no （don't let 'em in, shhh）",
+      image: "https://images-na.ssl-images-amazon.com/images/I/71RDpVmr2hL._SY355_.jpg",
+      created_at: "2019-10-04 05:52:53",
+    },
+    {
+      video_url: 'https://www.youtube.com/embed/K_9tX4eHztY',
+      title: 'future ELVO',
+      text: "A question that might come to mind when 
+      analyzing trap music is whether the lyrical content is in fact an
+      accurate representation of the rapper's selves and their views. 
+      With this question in mind, what can one make of Future?
       
-#       As his life fell apart in recent years, his music has 
-#       receded further and further away
+      As his life fell apart in recent years, his music has 
+      receded further and further away
       
-#       from human decency, with fugue-like beats and gruesome
-#       lyrics depicting himself as drugged out, violent, almost criminal.
-#       But as his music tumbled deeper into the abyss,
-#       the acclaim and success he found skyrocketed
+      from human decency, with fugue-like beats and gruesome
+      lyrics depicting himself as drugged out, violent, almost criminal.
+      But as his music tumbled deeper into the abyss,
+      the acclaim and success he found skyrocketed
       
-#       Future's character is both enthralling and disturbing:
-#       The content of his drawling verses will shock you—if you manage to pay attention.
-#       He is the super-villain incumbent of trap: Dark, brooding, and dangerous. ",
-#       image: "https://www.mcgilltribune.com/wp-content/uploads/2016/02/future-evol-new-album.jpg",
-#       created_at: "2019-10-05 05:52:53",
-#     },
-#     {
-#       video_url: 'https://www.youtube.com/embed/lXYq9ImJIN8',
-#       title: 'PartyNextDoor',
-#       text: "This what 'Sauga feels like in the night time
-#       Watch what she doin' when the light shine
-#       Drunk niggas tryna talk in the strip club
-#       Shawty silhouette looks like a dolla sign
-#       Caught up （caught up）
-#       Thats just how a nigga brought up （brought up）
-#       Blow ones for you loonie ass niggas
-#       Straight bills for you toonie ass niggas
-#       My niggas bigger then the bouncer
-#       Roll up in the bitch still smell like an ounce
-#       Right quick, right quick
-#       Tight jeans on so she feels my shit
-#       Tell me something good baby
-#       Tell me something, tell me something good shawty
-#       Come bring it to the hood baby
-#       Bring it, bring it back to hood",
-#       image: "https://e.snmc.io/i/300/w/3e55596c7bee19052c638d39fd722a9a/4839774",
-#       created_at: "2019-10-06 05:52:53",
-#     },
-#     {
-#       video_url: 'https://www.youtube.com/embed/R0ykLlhg0AQ',
-#       title: 'Money In The Grave',
-#       text: "Official “Money In The Grave” ft. Rick Ross Lyrics
+      Future's character is both enthralling and disturbing:
+      The content of his drawling verses will shock you—if you manage to pay attention.
+      He is the super-villain incumbent of trap: Dark, brooding, and dangerous. ",
+      image: "https://www.mcgilltribune.com/wp-content/uploads/2016/02/future-evol-new-album.jpg",
+      created_at: "2019-10-05 05:52:53",
+    },
+    {
+      video_url: 'https://www.youtube.com/embed/lXYq9ImJIN8',
+      title: 'PartyNextDoor',
+      text: "This what 'Sauga feels like in the night time
+      Watch what she doin' when the light shine
+      Drunk niggas tryna talk in the strip club
+      Shawty silhouette looks like a dolla sign
+      Caught up （caught up）
+      Thats just how a nigga brought up （brought up）
+      Blow ones for you loonie ass niggas
+      Straight bills for you toonie ass niggas
+      My niggas bigger then the bouncer
+      Roll up in the bitch still smell like an ounce
+      Right quick, right quick
+      Tight jeans on so she feels my shit
+      Tell me something good baby
+      Tell me something, tell me something good shawty
+      Come bring it to the hood baby
+      Bring it, bring it back to hood",
+      image: "https://e.snmc.io/i/300/w/3e55596c7bee19052c638d39fd722a9a/4839774",
+      created_at: "2019-10-06 05:52:53",
+    },
+    {
+      video_url: 'https://www.youtube.com/embed/R0ykLlhg0AQ',
+      title: 'Money In The Grave',
+      text: "Official “Money In The Grave” ft. Rick Ross Lyrics
 
-#       Yeah, okay
-#       Lil CC on the beat, mmm
-#       Yeah, yeah
+      Yeah, okay
+      Lil CC on the beat, mmm
+      Yeah, yeah
       
-#       I mean, where the fuck should I really even start?
-#       I got hoes that I'm keepin' in the dark
-#       I got my niggas 'cross the street livin' large
-#       Thinkin' back to the fact that they dead
-#       Thought my raps wasn't facts 'til they sat with the bars
-#       I got two phones, one need a charge
-#       Yeah, they twins, I could tell they ass apart
-#       I got big packs comin' on the way
-#       I got big stacks comin' out the safe
-#       I got Lil Max with me, he the wave
-#       It's a big gap between us in the game
-#       In the next life, I'm tryna stay paid
-#       When I die, put my money in the grave
+      I mean, where the fuck should I really even start?
+      I got hoes that I'm keepin' in the dark
+      I got my niggas 'cross the street livin' large
+      Thinkin' back to the fact that they dead
+      Thought my raps wasn't facts 'til they sat with the bars
+      I got two phones, one need a charge
+      Yeah, they twins, I could tell they ass apart
+      I got big packs comin' on the way
+      I got big stacks comin' out the safe
+      I got Lil Max with me, he the wave
+      It's a big gap between us in the game
+      In the next life, I'm tryna stay paid
+      When I die, put my money in the grave
       
-#       When I die, put my money in the grave
-#       I really gotta put a couple niggas in they place
-#       Really just lapped every nigga in the race
-#       I really might tat 'Realest Nigga' on my face
-#       Lil CC let it slap with the bass
-#       I used to save hoes with a mask and a cape
-#       Now I'm like, 'Nah, love, I'm good, go away'
-#       Ain't about to die with no money, I done gave it",
-#       image: "https://cdn.webshopapp.com/shops/134936/files/77085101/350x350x2/drake-views-cd.jpg",
-#       created_at: "2019-10-07 02:47:13",
-#     },
-#     {
-#       video_url: 'https://www.youtube.com/embed/MRyLC2M1K2w',
-#       title: 'Hollywood’s Bleeding',
-#       text: "Post Malone - Saint-Tropez
+      When I die, put my money in the grave
+      I really gotta put a couple niggas in they place
+      Really just lapped every nigga in the race
+      I really might tat 'Realest Nigga' on my face
+      Lil CC let it slap with the bass
+      I used to save hoes with a mask and a cape
+      Now I'm like, 'Nah, love, I'm good, go away'
+      Ain't about to die with no money, I done gave it",
+      image: "https://cdn.webshopapp.com/shops/134936/files/77085101/350x350x2/drake-views-cd.jpg",
+      created_at: "2019-10-07 02:47:13",
+    },
+    {
+      video_url: 'https://www.youtube.com/embed/MRyLC2M1K2w',
+      title: 'Hollywood’s Bleeding',
+      text: "Post Malone - Saint-Tropez
 
-#       Such a long time I’ve been waiting, I’ve been waiting for a long time
-#       Such a long time I’ve been waiting, I’ve been waiting for a long time
-#       Such a long time,
-#       ooh Ooh This shit bliss, I’m so rich Abs like Abercrombie Fitch Ooh Milly, 
-#       on my, whoa Versace boxers on my dick Bud Light runnin’ 
-#       through my piss On a yacht 50 meters 
-#       insuffish 50 carats on my fist The roof go down when I hit switch
+      Such a long time I’ve been waiting, I’ve been waiting for a long time
+      Such a long time I’ve been waiting, I’ve been waiting for a long time
+      Such a long time,
+      ooh Ooh This shit bliss, I’m so rich Abs like Abercrombie Fitch Ooh Milly, 
+      on my, whoa Versace boxers on my dick Bud Light runnin’ 
+      through my piss On a yacht 50 meters 
+      insuffish 50 carats on my fist The roof go down when I hit switch
       
-#       I moneyball like Bradley Pitt
-#       I worked so hard for all this shit
-#       Pumpin’ out classics
-#       In the Bat mobile goin’ bat shit
+      I moneyball like Bradley Pitt
+      I worked so hard for all this shit
+      Pumpin’ out classics
+      In the Bat mobile goin’ bat shit
       
-#       Such a long time
-#       I’ve been waiting, I’ve been waiting for a long time
-#       Such a long time
-#       I’ve been waiting, I’ve been waiting for a long time
-#       Such a long time
+      Such a long time
+      I’ve been waiting, I’ve been waiting for a long time
+      Such a long time
+      I’ve been waiting, I’ve been waiting for a long time
+      Such a long time
       
-#       I’m in Saint-Tropez I had to check wrist
-#       I just bought my girl a new necklace
-#       1, 2, 3, 4, 5, 6
-#       I’ll take em all don’t matter what the price is
+      I’m in Saint-Tropez I had to check wrist
+      I just bought my girl a new necklace
+      1, 2, 3, 4, 5, 6
+      I’ll take em all don’t matter what the price is
       
-#       I said I’m sorry mama for my vices
-#       You’ll never understand what my life is
-#       1, 2, 3, 4, 5, 6
-#       Shit I’m checking off my bucket list
+      I said I’m sorry mama for my vices
+      You’ll never understand what my life is
+      1, 2, 3, 4, 5, 6
+      Shit I’m checking off my bucket list
       
-#       You try to give advice I don’t need it
-#       I’ve been doin’ what I want since fetus
+      You try to give advice I don’t need it
+      I’ve been doin’ what I want since fetus
       
-#       What you call a holiday
-#       I call another day
-#       And I ain’t ever stoppin’ no apologies
+      What you call a holiday
+      I call another day
+      And I ain’t ever stoppin’ no apologies
       
-#       Such a long time
-#       I’ve been waiting, I’ve been waiting for a long time
-#       Such a long time
-#       I’ve been waiting, I’ve been waiting for a long time
-#       Such a long time
+      Such a long time
+      I’ve been waiting, I’ve been waiting for a long time
+      Such a long time
+      I’ve been waiting, I’ve been waiting for a long time
+      Such a long time
       
-#       I’m in Saint-Tropez I had to check wrist",
-#       image: "https://t2.genius.com/unsafe/440x0/https%3A%2F%2Fimages.genius.com%2F456a385f5f9741e2202331ac8d06e1f2.1000x1000x1.jpg",
-#       created_at: "2019-10-08 05:52:53",
-#     },
-#   ]
-# )
+      I’m in Saint-Tropez I had to check wrist",
+      image: "https://t2.genius.com/unsafe/440x0/https%3A%2F%2Fimages.genius.com%2F456a385f5f9741e2202331ac8d06e1f2.1000x1000x1.jpg",
+      created_at: "2019-10-08 05:52:53",
+    },
+  ]
+)
