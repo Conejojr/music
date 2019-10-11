59c3f677ee0e2515575fb79f47ef7c910fd77df5
kiyoshi <kiyo7s@icloud.com>
2019-10-11 20:13:29 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -221,3 +221,108 @@ Blog.create!(
     },
   ]
 )
+
+Tune.create!(
+  [
+    {
+      img_url: "/scale/rankinfo/00000143/202x_image143838.jpg"
+      music_title: "Truth Hurts"
+      artist_name: "Lizzo"
+    },
+    {
+      img_url: "/scale/rankinfo/00000145/202x_image145749.jpg"
+      music_title: "Senorita"
+      artist_name: "Shawn Mendes & Camila Cabello	"
+    },
+    {
+      img_url: "/scale/jackets/00000124/202x_P2_G8383335W.JPG"
+      music_title: "Bad Guy"
+      artist_name: "Billie Eilish"
+    },
+    {
+      img_url: "/scale/common/202x_img_noimage.png"
+      music_title: "Ran$om"
+      artist_name: "Lil Tecca"
+    },
+    {
+      img_url: "/scale/rankinfo/00000142/202x_image142881.jpg"
+      music_title: "Old Town Road"
+      artist_name: "Lil Nas X Featuring Billy Ray Cyrus"
+    },
+    {
+      img_url: "/scale/jackets/00000127/202x_P2_G8395415W.JPG"
+      music_title: "No Guidance"
+      artist_name: "Chris Brown Featuring Drake"
+    },
+    {
+      img_url: "/scale/rankinfo/00000148/202x_image148414.png"
+      music_title: "Circles"
+      artist_name: "Post Malone"
+    },
+    {
+      img_url: "/scale/jackets/00000126/202x_P2_G6405668W.JPG"
+      music_title: "I Don't Care"
+      artist_name: "Ed Sheeran & Justin Bieber"
+    },
+    {
+      img_url: "/scale/jackets/00000125/202x_P2_G6404666W.JPG"
+      music_title: "Talk"
+      artist_name: "Khalid"
+    },
+    {
+      img_url: "/scale/rankinfo/00000146/202x_image146253.png"
+      music_title: "Goodbyes"
+      artist_name: "Post Malone Featuring Young Thug"
+    },
+    {
+      img_url: "/scale/common/202x_img_noimage.png"
+      music_title: "Someone You Loved"
+      artist_name: "Lewis Capaldi"
+    },
+    {
+      img_url: "/scale/jackets/00000126/202x_P2_G8386582W.JPG"
+      music_title: "You Need To Calm Down"
+      artist_name: "Taylor Swift"
+    },
+    {
+      img_url: "/scale/jackets/00000126/202x_P2_G6405451W.JPG"
+      music_title: "Sucker"
+      artist_name: "Jonas Brothers"
+    },
+    {
+      img_url: "/scale/rankinfo/00000136/202x_image136622.jpg"
+      music_title: "Sunflower"
+      artist_name: "Post Malone & Swae Lee"
+    },
+    {
+      img_url: "/scale/common/202x_img_noimage.png"
+      music_title: "Money In The Grave"
+      artist_name: "Drake Featuring Rick Ross"
+    },
+    {
+      img_url: "/scale/rankinfo/00000143/202x_image143656.jpg"
+      music_title: "If I Can't Have You	"
+      artist_name: "Shawn Mendes"
+    },
+    {
+      img_url: "/scale/jackets/00000126/202x_P2_G6405668W.JPG"
+      music_title: "Beautiful People"
+      artist_name: "Ed Sheeran Featuring Khalid"
+    },
+    {
+      img_url: "/scale/common/202x_img_noimage.png"
+      music_title: "Suge"
+      artist_name: "DaBaby"
+    },
+    {
+      img_url: "/scale/common/202x_img_noimage.png"
+      music_title: "Boyfriend"
+      artist_name: "Ariana Grande & Social House"
+    },
+    {
+      img_url: "/scale/common/202x_img_noimage.png"
+      music_title: "The Git Up"
+      artist_name: "Blanco Brown"
+    },
+  ]
+)
